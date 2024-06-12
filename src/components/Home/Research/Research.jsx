dff82bd53c8c54e696718f0f89aeea9fae19462e
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-06-12 09:42:56 +0600

    research data fethc done

--- src/components/Home/Research/Research.jsx
+++ src/components/Home/Research/Research.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+
 const Research = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../../../../public/research.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>this is research page</h1>
+      <h1 className="text-3xl font-semibold">This is the research page</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.name}</h2>
+            <img src={item.image} alt={item.name} />
+            <p>{item.description}</p>
+            <p>Role: {item.value}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/Home/Shared/RouteItem.jsx
+++ src/components/Home/Shared/RouteItem.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const RouteItem = () => {
   return (
     <>
-      <div className="">
+      <div className="my-8">
         <div className="">
-          <ul className="flex gap-4 font-semibold p-3 bg-green-200 ">
+          <ul className="flex gap-4 font-semibold p-3 bg-black text-white  ">
             <li>
               <Link to="/research">Research</Link>
             </li>
